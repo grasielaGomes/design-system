d03d470991636fd355772f5a8eecb02a4994a2cb
Grasiela Gomes <produtoradama@gmail.com>
2022-10-20 17:36:12 -0300

    feat: add colors tokens

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  white: "#FFF",
+  black: "#000",
+
+  gray100: "#E5E5E5",
+  gray200: "#B2B2B2",
+  gray500: "#4D4D4D",
+
+  primary100: "#F4FBF9",
+  primary500: "#03D69D",
+
+  secondary: "#133A6F",
+};
