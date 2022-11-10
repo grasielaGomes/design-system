0ea2d8358b5ba0cef1a9636157a9940089761e4f
Grasiela Gomes <produtoradama@gmail.com>
2022-11-10 08:35:32 -0300

    refactor: change export names

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,36 @@
+import { createStitches, defaultThemeMap } from '@stitches/react'
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from '@ds-ui/tokens'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+  theme: {
+    colors,
+    fonts,
+    fontWeights,
+    fontSizes,
+    lineHeights,
+    radii,
+    space,
+  },
+})
