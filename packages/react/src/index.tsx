a2222068a9ef2c11383e1b3e5d759dbad9c53295
Grasiela Gomes <produtoradama@gmail.com>
2022-11-11 19:19:42 -0300

    feat: add Button

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,1 +1,32 @@
-export const hello = 'yeah!'
+import { ComponentProps } from 'react'
+import { styled } from './styles'
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  color: '$white',
+  backgroundColor: '$primary700',
+  borderRadius: '$sm',
+
+  variants: {
+    size: {
+      sm: {
+        fontSize: 12,
+        padding: '$1 $2',
+      },
+      md: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      lg: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'md',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
