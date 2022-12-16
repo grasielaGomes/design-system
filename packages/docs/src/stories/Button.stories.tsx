397f2d47590e8d80627faa64bde600d0428cc878
Grasiela Gomes <produtoradama@gmail.com>
2022-12-16 17:51:55 -0300

    docs: add tokens stories

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Button, ButtonProps } from '@ds-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
   args: {
     children: 'Button',
