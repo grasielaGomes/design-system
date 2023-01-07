493a058329301de2fd106512a7c50f924b701cbb
Grasiela Gomes <produtoradama@gmail.com>
2023-01-07 10:30:21 -0300

    feat: add argTypes

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -8,11 +8,32 @@ export default {
     children:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro reprehenderit est veniam? Rem recusandae sint perspiciatis debitis corrupti. Deserunt neque exercitationem illo voluptatum laudantium culpa suscipit accusantium blanditiis voluptas numquam.',
   },
+  argTypes: {
+    size: {
+      control: 'select',
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+    },
+    color: { control: 'select', options: ['primary', 'secondary', 'tertiary'] },
+  },
 } as Meta<TextProps>
 
 export const Primary: StoryObj<TextProps> = {}
 
-export const CustomTag: StoryObj<TextProps> = {
+export const CustomHTMLTag: StoryObj<TextProps> = {
   args: {
     children: 'Strong text',
     as: 'strong',
