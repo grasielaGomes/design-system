4436726d4bbfca85565ee63959564441031660d4
Grasiela Gomes <produtoradama@gmail.com>
2023-01-07 09:47:55 -0300

    feat: add Heading

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Heading, HeadingProps } from '@ds-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Lorem ipsum',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 title',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'You can use any HTML tag as a heading',
+      },
+    },
+  },
+}
